7c1dbf6ee4f5d75d993a74d01b30cef741b787d4
jbout <jbout@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-09-10 09:13:07 +0000

    keep former compilations for active delivery executions
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/lti/ltiDeliveryProvider/trunk@15884 62dfeb86-a93f-4914-9d13-15c0a160985d

--- actions/class.DeliveryLinks.php
+++ actions/class.DeliveryLinks.php
@@ -58,7 +58,7 @@ class ltiDeliveryProvider_actions_DeliveryLinks extends taoLti_actions_LinkManag
             $feedBackMessage = __("Notice : In order to use this delivery from an external LTI consumer tool, you will need to configure it with a LTI Basic outcome result server");
         }
         
-        $compiledDelivery = taoDelivery_models_classes_CompilationService::singleton()->getCompiledContent($selectedDelivery);
+        $compiledDelivery = taoDelivery_models_classes_CompilationService::singleton()->getActiveCompilation($selectedDelivery);
         if (is_null($compiledDelivery)) {
             $feedBackMessage = __('Delivery has not been compiled yet');
         } else {
