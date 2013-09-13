9eb122b3d13a14166adc224a07a001dc12bd3a49
pplichart <pplichart@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-09-13 12:35:36 +0000

    add a storagee to the result server init
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/lti/ltiDeliveryProvider/trunk@16102 62dfeb86-a93f-4914-9d13-15c0a160985d

--- models/classes/class.LTIDeliveryTool.php
+++ models/classes/class.LTIDeliveryTool.php
@@ -93,7 +93,7 @@ class ltiDeliveryProvider_models_classes_LTIDeliveryTool extends taoLti_models_c
 
         $delivery = taoDelivery_models_classes_DeliveryServerService::singleton()->getDeliveryFromCompiledDelivery($compiledDelivery);
 	    //The result server from LTI context depend on call parameters rather than static result server definition
-	    $this->initLtiResultServer($delivery, $deliveryExecution);
+	    ltiDeliveryProvider_helpers_ResultServer::initLtiResultServer($delivery, $deliveryExecution);
 	    // lis_outcome_service_url This value should not change from one launch to the next and in general,
         //  the TP can expect that there is a one-to-one mapping between the lis_outcome_service_url and a particular oauth_consumer_key.  This value might change if there was a significant re-configuration of the TC system or if the TC moved from one domain to another.
         return $deliveryExecution;
@@ -101,47 +101,6 @@ class ltiDeliveryProvider_models_classes_LTIDeliveryTool extends taoLti_models_c
 	}
 	
 
-	private function initLtiResultServer(core_kernel_classes_Resource $delivery, $deliveryExecution) {
-	    $launchData = taoLti_models_classes_LtiService::singleton()->getLtiSession()->getLaunchData();
-	    $resultServerCallOptions = array(
-            array(
-	        "implementation" =>"taoLtiBasicOutcome_models_classes_LtiBasicOutcome",
-	        "parameters" => array(
-                "result_identifier" => $launchData->getVariable("lis_result_sourcedid"),
-                "consumer_key" => $launchData->getOauthKey(),
-                "service_url" => $launchData->getVariable("lis_outcome_service_url"),
-                "user_identifier" => common_session_SessionManager::getSession()->getUserUri()
-                )
-            )
-	    );
-	    //starts or resume a taoResultServerStateFull session for results submission
 	
-	    //retrieve the resultServer definition that is related to this delivery to be used
-	    
-	    //retrieve the result server definition
-	    try {
-        $resultServer = $delivery->getUniquePropertyValue(new core_kernel_classes_Property(TAO_DELIVERY_RESULTSERVER_PROP));
-        } catch (Exception $e) {
-            //No static result server was associated with the delivery
-            $resultServer = new core_kernel_classes_Resource(TAO_VOID_RESULT_SERVER);
-        }
-	    //callOptions are required in the case of a LTI basic storage
-
-	    taoResultServer_models_classes_ResultServerStateFull::singleton()->initResultServer($resultServer->getUri(), $resultServerCallOptions);
-	
-	    //a unique identifier for data collected through this delivery execution
-	    //in the case of LTI, we should use the sourceId
-	    $resultIdentifier = (isset($resultServerCallOptions["result_identifier"])) ? $resultServerCallOptions["result_identifier"] :$deliveryExecution->getUri();
-	    //the dependency to taoResultServer should be re-thinked with respect to a delivery level proxy
-	    taoResultServer_models_classes_ResultServerStateFull::singleton()->spawnResult($deliveryExecution->getUri(), $resultIdentifier);
-	    common_Logger::i("Spawning".$resultIdentifier ."related to process execution ".$deliveryExecution->getUri());
-	    $userIdentifier = (isset($resultServerCallOptions["user_identifier"])) ? $resultServerCallOptions["user_identifier"] :wfEngine_models_classes_UserService::singleton()->getCurrentUser()->getUri();
-	    //set up the related test taker
-	    //a unique identifier for the test taker
-	    taoResultServer_models_classes_ResultServerStateFull::singleton()->storeRelatedTestTaker( $userIdentifier);
-	
-	    //a unique identifier for the delivery
-	    taoResultServer_models_classes_ResultServerStateFull::singleton()->storeRelatedDelivery($delivery->getUri());
-	}
 	
 }
