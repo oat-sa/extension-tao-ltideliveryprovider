337035a469251111a5bc01f2921200cbdcce7a2e
jbout <jbout@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-01-06 15:17:19 +0000

    Prevent exception if consumer does not provide outcome service
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/lti/ltiDeliveryProvider/trunk@17747 62dfeb86-a93f-4914-9d13-15c0a160985d

--- helpers/class.ResultServer.php
+++ helpers/class.ResultServer.php
@@ -24,43 +24,42 @@ class ltiDeliveryProvider_helpers_ResultServer
 {
 
     public static function initLtiResultServer(core_kernel_classes_Resource $delivery, core_kernel_classes_Resource $deliveryExecution, $launchData) {
-	      $storageImplFromLaunch = array(
-            array(
-	        "implementation" =>"taoLtiBasicOutcome_models_classes_LtiBasicOutcome",
-	        "parameters" => array(
-                "result_identifier" => $launchData->getVariable("lis_result_sourcedid"),
-                "consumer_key" => $launchData->getOauthKey(),
-                "service_url" => $launchData->getVariable("lis_outcome_service_url"),
-                "user_identifier" => common_session_SessionManager::getSession()->getUserUri(),
-	            "user_fullName" => ($launchData->hasVariable(taoLti_models_classes_LtiLaunchData::LIS_PERSON_NAME_FULL)
-	                ? $launchData->getVariable(taoLti_models_classes_LtiLaunchData::LIS_PERSON_NAME_FULL)
-	                : '')
-                )
-            )
-	    );
-
-	    try {
-        $resultServer = $delivery->getUniquePropertyValue(new core_kernel_classes_Property(TAO_DELIVERY_RESULTSERVER_PROP));
-        } catch (Exception $e) {
+        
+        $resultServer = $delivery->getOnePropertyValue(new core_kernel_classes_Property(TAO_DELIVERY_RESULTSERVER_PROP));
+        if (empty($resultServer)) {
             //No static result server was associated with the delivery
             $resultServer = new core_kernel_classes_Resource(TAO_VOID_RESULT_SERVER);
         }
+        
+        if ($launchData->hasVariable("lis_result_sourcedid") && $launchData->hasVariable("lis_outcome_service_url")) {
+                
+            $options = array(
+                array(
+    	        "implementation" =>"taoLtiBasicOutcome_models_classes_LtiBasicOutcome",
+    	        "parameters" => array(
+                    "result_identifier" => $launchData->getVariable("lis_result_sourcedid"),
+                    "consumer_key" => $launchData->getOauthKey(),
+                    "service_url" => $launchData->getVariable("lis_outcome_service_url"),
+                    "user_identifier" => common_session_SessionManager::getSession()->getUserUri(),
+    	            "user_fullName" => ($launchData->hasVariable(taoLti_models_classes_LtiLaunchData::LIS_PERSON_NAME_FULL)
+    	                ? $launchData->getVariable(taoLti_models_classes_LtiLaunchData::LIS_PERSON_NAME_FULL)
+    	                : '')
+                    )
+                )
+    	    );
+    	    $resultIdentifier = $launchData->getVariable("lis_result_sourcedid");
+        } else {
+            $options = array();
+            $resultIdentifier = $deliveryExecution->getUri();
+        }
 
-        /*todo manage result dientifier per storage */
-        $storage = current($storageImplFromLaunch);
-        $storageParameters = $storage["parameters"];
-        $launchResultIdentifier = $storageParameters["result_identifier"];
-        $launchUserIdentifier = ($storageParameters["user_identifier"]=="") ? $storageParameters["user_identifier"] : $storageParameters["user_fullName"];
-       
-
-	    taoResultServer_models_classes_ResultServerStateFull::singleton()->initResultServer($resultServer->getUri(), $storageImplFromLaunch);
-	    $resultIdentifier = (isset($launchResultIdentifier)) ? $launchResultIdentifier :$deliveryExecution->getUri();
-	    taoResultServer_models_classes_ResultServerStateFull::singleton()->spawnResult($deliveryExecution->getUri(), $resultIdentifier);
-	    common_Logger::i("Spawning".$resultIdentifier ."related to process execution ".$deliveryExecution->getUri());
-	    $userIdentifier = (isset($launchUserIdentifier)) ? $launchUserIdentifier :wfEngine_models_classes_UserService::singleton()->getCurrentUser()->getUri();
-	  
-        taoResultServer_models_classes_ResultServerStateFull::singleton()->storeRelatedTestTaker( $userIdentifier);
-	    taoResultServer_models_classes_ResultServerStateFull::singleton()->storeRelatedDelivery($delivery->getUri());
+        common_Logger::i("Spawning '".$resultIdentifier ."' related to delivery execution ".$deliveryExecution->getUri());
+        $resultServerState = taoResultServer_models_classes_ResultServerStateFull::singleton(); 
+        $resultServerState->initResultServer($resultServer->getUri(), $options);
+        $resultServerState->spawnResult($deliveryExecution->getUri(), $resultIdentifier);
+         
+        $resultServerState->storeRelatedTestTaker(common_session_SessionManager::getSession()->getUserUri());
+        $resultServerState->storeRelatedDelivery($delivery->getUri());
 	}
 
 
