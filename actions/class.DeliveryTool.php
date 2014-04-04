b8bfc45e5c829ad35ca40288479117c9e9e73603
llecaque <llecaque@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-04-04 16:14:44 +0000

    update header for phpdoc remove old argo stub
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/lti/ltiDeliveryProvider/trunk@19446 62dfeb86-a93f-4914-9d13-15c0a160985d

--- actions/class.DeliveryTool.php
+++ actions/class.DeliveryTool.php
@@ -24,7 +24,7 @@
  * @author CRP Henri Tudor - TAO Team - {@link http://www.tao.lu}
  * @license GPLv2  http://www.opensource.org/licenses/gpl-2.0.php
  * @package ltiDeliveryProvider
- * @subpackage actions
+ 
  */
 class ltiDeliveryProvider_actions_DeliveryTool extends taoLti_actions_ToolModule
 {
