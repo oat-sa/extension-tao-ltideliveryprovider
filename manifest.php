f0ec779a1d82c6c9e03d594bdf16dbde26d3aee6
Lionel Lecaque <lionel@taotesting.com>
2015-06-18 16:30:11 +0200

    increase version number + update script

--- manifest.php
+++ manifest.php
@@ -28,7 +28,7 @@ return array(
     'label' => 'LTI Delivery Tool Provider',
 	'description' => 'The LTI Delivery Tool Provider allows third party applications to embed deliveries created in Tao',
     'license' => 'GPL-2.0',
-    'version' => '1.0',
+    'version' => '1.0.1',
 	'author' => 'Open Assessment Technologies',
 	'requires' => array(
 	    'taoDelivery' => '>=2.6',
@@ -47,6 +47,7 @@ return array(
     'routes' => array(
         '/ltiDeliveryProvider' => 'oat\\ltiDeliveryProvider\\controller'
     ),
+    'update' => 'oat\\ltiDeliveryProvider\\scripts\\update\\Updater',
     'managementRole' => 'http://www.tao.lu/Ontologies/TAOLTI.rdf#LtiDeliveryProviderManagerRole',
     'acl' => array(
         array('grant', 'http://www.tao.lu/Ontologies/TAOLTI.rdf#LtiDeliveryProviderManagerRole', array('ext'=>'ltiDeliveryProvider')),
