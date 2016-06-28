14712b2e935f0de05de1e6afa1bfc6bd69d32d63
christophe garcia <christophe.garcia@taotesting.com>
2016-06-28 15:00:30 +0200

    LTI launch URL REST API

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -20,6 +20,9 @@
  */
 namespace oat\ltiDeliveryProvider\scripts\update;
 
+use oat\tao\model\accessControl\func\AclProxy;
+use oat\tao\model\accessControl\func\AccessRule;
+
 class Updater extends \common_ext_ExtensionUpdater
 {
 
@@ -28,6 +31,7 @@ class Updater extends \common_ext_ExtensionUpdater
      */
     public function update($initialVersion)
     {
-        $this->skip('0', '1.3.1');
+        $this->skip('0', '1.4.0');
+        
     }
 }
