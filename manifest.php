46dbe2219c74d14eff87dafcfb3199df2322e5a8
Aleh Hutnikau <hutnikau@1pt.com>
2017-04-05 17:32:35 +0300

    return lti message after finishing of delivrey execution

--- manifest.php
+++ manifest.php
@@ -29,7 +29,7 @@ return array(
     'label' => 'LTI Delivery Tool Provider',
     'description' => 'The LTI Delivery Tool Provider allows third party applications to embed deliveries created in Tao',
     'license' => 'GPL-2.0',
-    'version' => '2.4.2',
+    'version' => '2.5.0',
     'author' => 'Open Assessment Technologies',
     'requires' => array(
         'tao' => '>=7.45.5',
