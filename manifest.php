4904377b2c8210f7c93a977e69f8f1142b968e32
Aleh Hutnikau <hutnikau@1pt.com>
2017-10-12 11:18:26 +0300

    update dependency

--- manifest.php
+++ manifest.php
@@ -29,7 +29,7 @@ return array(
     'version' => '3.6.0',
     'author' => 'Open Assessment Technologies',
     'requires' => array(
-        'tao' => '>=9.0.0',
+        'tao' => '>=13.2.0',
         'taoDeliveryRdf' => '>=1.0',
         'taoLti' => '>=3.2.2',
         'taoLtiBasicOutcome' => '>=2.6',
