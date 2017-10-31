dba243e4f8fb18cb78020ad9b4ce2aab75c6700e
Aleh Hutnikau <hutnikau@1pt.com>
2017-10-31 16:20:41 +0300

    Use result server service

--- manifest.php
+++ manifest.php
@@ -27,7 +27,7 @@ return array(
     'label' => 'LTI Delivery Tool Provider',
     'description' => 'The LTI Delivery Tool Provider allows third party applications to embed deliveries created in Tao',
     'license' => 'GPL-2.0',
-    'version' => '3.7.2',
+    'version' => '3.8.0',
     'author' => 'Open Assessment Technologies',
     'requires' => array(
         'generis' => '>=5.2.0',
@@ -35,8 +35,8 @@ return array(
         'taoDeliveryRdf' => '>=1.0',
         'taoLti' => '>=3.2.2',
         'taoLtiBasicOutcome' => '>=2.6',
-        'taoResultServer' => '>=3.4.0',
-        'taoDelivery' => '>=7.0.0'
+        'taoResultServer' => '>=4.2.0',
+        'taoDelivery' => '>=7.5.0'
     ),
     'models' => array(
          'http://www.tao.lu/Ontologies/TAOLTI.rdf',
