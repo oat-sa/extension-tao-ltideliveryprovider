0bb2c29a34103bdbc982ecfe23f3354d1b22796f
Ionut Padurariu <ionut@taotesting.com>
2018-01-22 10:04:29 +0100

    fix version

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -173,6 +173,6 @@ class Updater extends \common_ext_ExtensionUpdater
             $this->setVersion('4.0.0');
         }
 
-        $this->skip('4.0.0', '4.6.0');
+        $this->skip('4.0.0', '4.2.0');
     }
 }
