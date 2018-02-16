72733ba44c7a7299d297fdc1802c125f84306a3d
siwane <camille@taotesting.com>
2018-02-16 17:16:04 +0100

    Update dependency

--- manifest.php
+++ manifest.php
@@ -33,7 +33,7 @@ return array(
         'generis' => '>=5.2.0',
         'tao' => '>=15.11.0',
         'taoDeliveryRdf' => '>=1.0',
-        'taoLti' => '>=4.2.0',
+        'taoLti' => '>=5.0.0',
         'taoResultServer' => '>=5.0.0',
         'taoDelivery' => '>=9.0.0'
     ),
