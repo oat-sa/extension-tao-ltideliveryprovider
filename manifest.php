69a4fa8b9edc34b6eb9f1531b33fd61657a71e56
Péter Halász <halaszpeterzsolt@gmail.com>
2018-09-26 15:46:26 +0200

    bump version

--- manifest.php
+++ manifest.php
@@ -27,7 +27,7 @@ return array(
     'label' => 'LTI Delivery Tool Provider',
     'description' => 'The LTI Delivery Tool Provider allows third party applications to embed deliveries created in Tao',
     'license' => 'GPL-2.0',
-    'version' => '6.5.3',
+    'version' => '6.5.4',
     'author' => 'Open Assessment Technologies',
     'requires' => array(
         'generis' => '>=7.1.0',
