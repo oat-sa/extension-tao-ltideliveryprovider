8f5106ccb2568ae8e2955ee708de004275f8f805
sergii <panua01@gmail.com>
2019-08-06 12:40:32 +0200

    tests added

---
+++ model/LtiDeliveryFactory.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2019 (original work) Open Assessment Technologies SA;
+ *
+ */
+
+namespace oat\ltiDeliveryProvider\model;
+
+
+use core_kernel_classes_Container;
+use core_kernel_classes_Resource;
+use oat\generis\model\OntologyAwareTrait;
+use oat\oatbox\service\ConfigurableService;
+
+class LtiDeliveryFactory extends ConfigurableService
+{
+    use OntologyAwareTrait;
+
+    const SERVICE_ID = 'ltiDeliveryProvider/LtiDeliveryFactory';
+
+    /**
+     * @param $uri
+     *
+     * @return core_kernel_classes_Container|core_kernel_classes_Resource
+     */
+    public function create($uri)
+    {
+        return $uri !== null
+            ? $this->getResource($uri)
+            : LTIDeliveryTool::singleton()->getDeliveryFromLink();
+    }
+}
