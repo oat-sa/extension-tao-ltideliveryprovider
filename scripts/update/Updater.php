244402edfc41bf5fbef8ab229ee2e72402027dec
Oleksandr Khitko <oleksandr@taotesting.com>
2020-02-27 15:57:22 +0100

    Bump version

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -324,6 +324,6 @@ class Updater extends \common_ext_ExtensionUpdater
             $this->getServiceManager()->register(LTIDeliveryToolFactory::SERVICE_ID, new LTIDeliveryToolFactory());
             $this->setVersion('9.4.0');
         }
-        $this->skip('9.4.0', '10.1.6');
+        $this->skip('9.4.0', '10.2.0');
     }
 }
