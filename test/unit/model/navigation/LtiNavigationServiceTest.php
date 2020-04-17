e281d9378bfc0fda3ae4a64fa127ae9b1e642c73
Oleksandr Khitko <oleksandr@taotesting.com>
2020-04-17 20:16:39 +0200

    Cover LtiNavigationService with tests before refactoring

--- test/unit/model/navigation/LtiNavigationServiceTest.php
+++ test/unit/model/navigation/LtiNavigationServiceTest.php
@@ -20,11 +20,17 @@
 
 namespace oat\ltiDeliveryProvider\test\unit\model\navigation;
 
+use oat\ltiDeliveryProvider\model\navigation\DefaultMessageFactory;
 use oat\ltiDeliveryProvider\model\navigation\LtiNavigationService;
 use oat\generis\test\TestCase;
+use oat\oatbox\log\LoggerService;
+use oat\tao\helpers\UrlHelper;
 use oat\taoDelivery\model\execution\DeliveryExecutionInterface;
 use oat\taoLti\models\classes\LtiLaunchData;
 use oat\generis\test\MockObject;
+use oat\taoLti\models\classes\LtiMessages\LtiMessage;
+use Psr\Log\LoggerInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
 
 /**
  * Class LtiNavigationServiceTest
@@ -32,6 +38,10 @@ use oat\generis\test\MockObject;
  */
 class LtiNavigationServiceTest extends TestCase
 {
+    private const THANK_YOU_URL = 'http://THANK_YOU/';
+
+    private const DELIVERY_EXECUTION_ID = 'FAKE_DELIVERY_EXECUTION_ID';
+
     /**
      * @var LtiNavigationService|MockObject
      */
@@ -48,21 +58,44 @@ class LtiNavigationServiceTest extends TestCase
     private $deliveryExecutionMock;
 
     /**
+     * @var DefaultMessageFactory|MockObject
+     */
+    private $messageFactoryMock;
+
+    /**
+     * @var UrlHelper|MockObject
+     */
+    private $urlHelperMock;
+
+    /**
+     * @var LtiMessage|MockObject
+     */
+    private $ltiMessageMock;
+
+    /**
      * @inheritdoc
      */
     protected function setUp(): void
     {
         parent::setUp();
 
-        $this->object = $this->getMockBuilder(LtiNavigationService::class)
-            ->setMethods(['getThankYouUrl'])
-            ->getMock();
+        // Mock method call parameters
+        $this->launchDataMock = $this->createMock(LtiLaunchData::class);
 
-        $this->launchDataMock = $this->getMockBuilder(LtiLaunchData::class)
-            ->disableOriginalConstructor()
-            ->getMock();
-        $this->deliveryExecutionMock = $this->getMockBuilder(DeliveryExecutionInterface::class)
-            ->getMock();
+        $this->deliveryExecutionMock = $this->createMock(DeliveryExecutionInterface::class);
+        $this->deliveryExecutionMock->method('getIdentifier')
+            ->willReturn(self::DELIVERY_EXECUTION_ID);
+
+        $this->messageFactoryMock = $this->getMessageFactoryMock();
+
+        $this->object = new LtiNavigationService([
+            LtiNavigationService::OPTION_THANK_YOU_SCREEN => false,
+            LtiNavigationService::OPTION_DELIVERY_RETURN_STATUS => false,
+            LtiNavigationService::OPTION_MESSAGE_FACTORY => $this->messageFactoryMock
+        ]);
+
+        $serviceLocatorMock = $this->createServiceLocatorMock();
+        $this->object->setServiceLocator($serviceLocatorMock);
     }
 
     /**
@@ -70,17 +103,100 @@ class LtiNavigationServiceTest extends TestCase
      */
     public function testGetReturnUrlNoReturnUrl()
     {
-        $expectedUrl = 'http://THANK_YOU/';
-
         $this->launchDataMock->expects($this->once())
             ->method('hasReturnUrl')
             ->willReturn(false);
-        $this->object->expects($this->once())
-            ->method('getThankYouUrl')
-            ->willReturn($expectedUrl);
 
         $result = $this->object->getReturnUrl($this->launchDataMock, $this->deliveryExecutionMock);
 
-        $this->assertEquals($expectedUrl, $result, 'Returned url must be as expected for request without return url.');
+        static::assertSame(self::THANK_YOU_URL, $result, 'Returned url must be as expected for request without return url.');
+    }
+
+    /**
+     * @param bool $thankYouScreenOption
+     * @param string $ltiReturnUrl
+     * @param string $expectedUrl
+     *
+     * @dataProvider dataProviderTestGetReturnUrl_NoSkipThankYouParameter
+     */
+    public function testGetReturnUrl_NoSkipThankYouParameter(bool $thankYouScreenOption, string $ltiReturnUrl, string $expectedUrl): void
+    {
+        $this->launchDataMock->method('hasReturnUrl')
+            ->willReturn(true);
+        $this->launchDataMock->method('getReturnUrl')
+            ->willReturn($ltiReturnUrl);
+
+        $this->launchDataMock->method('hasVariable')
+            ->with('custom_skip_thankyou')
+            ->willReturn(false);
+
+        $this->object->setOption('thankyouScreen', $thankYouScreenOption);
+        $result = $this->object->getReturnUrl($this->launchDataMock, $this->deliveryExecutionMock);
+
+        static::assertSame($expectedUrl, $result, 'Method must return correct "return url"');
+    }
+
+    public function testGetReturnUrl_SkipThankYouParameter_False(): void
+    {
+        $this->assertTrue(false);
+    }
+
+    public function testGetReturnUrl_SkipThankYouParameter_True(): void
+    {
+        $this->assertTrue(false);
+    }
+
+    public function testGetReturnUrl_SkipThankYouParameter_InvalidType(): void
+    {
+        $this->assertTrue(false);
+    }
+
+    public function dataProviderTestGetReturnUrl_NoSkipThankYouParameter(): array
+    {
+        return [
+            'Option show thank you screen true' => [
+                'thankYouScreenOption' => true,
+                'ltiReturnUrl' => 'http://FAKE_LTI_RETURN.URL',
+                'expectedUrl' => self::THANK_YOU_URL
+            ],
+            'Option show thank you screen false' => [
+                'thankYouScreenOption' => false,
+                'ltiReturnUrl' => 'http://FAKE_LTI_RETURN.URL',
+                'expectedUrl' => 'http://FAKE_LTI_RETURN.URL?deliveryExecution=' . self::DELIVERY_EXECUTION_ID
+            ]
+        ];
+    }
+
+    /**
+     * @return ServiceLocatorInterface|MockObject
+     */
+    private function createServiceLocatorMock(): ServiceLocatorInterface
+    {
+        $this->urlHelperMock = $this->createMock(UrlHelper::class);
+        $this->urlHelperMock->method('buildUrl')
+            ->willReturn(self::THANK_YOU_URL);
+
+        $loggerMock = $this->createMock(LoggerInterface::class);
+
+        $serviceLocatorMock = $this->getServiceLocatorMock(
+            [
+                UrlHelper::class => $this->urlHelperMock,
+                LoggerService::SERVICE_ID => $loggerMock
+            ]
+        );
+        return $serviceLocatorMock;
+    }
+
+    private function getMessageFactoryMock(): DefaultMessageFactory
+    {
+        $this->ltiMessageMock = $this->createMock(LtiMessage::class);
+        $this->ltiMessageMock->method('getUrlParams')
+            ->willReturn([]);
+
+        $messageFactoryMock = $this->createMock(DefaultMessageFactory::class);
+        $messageFactoryMock->method('getLtiMessage')
+            ->willReturn($this->ltiMessageMock);
+
+        return $messageFactoryMock;
     }
 }
