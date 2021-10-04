3dc8d34fdc2d48a86a4aef5167f4ac43dba54ac3
Eugene Golubev <emgolubev@gmail.com>
2021-10-04 13:11:18 +0300

    fix: typo

--- test/unit/model/LTIDeliveryToolTest.php
+++ test/unit/model/LTIDeliveryToolTest.php
@@ -61,7 +61,7 @@ class LTIDeliveryToolTest extends TestCase
         $this->subject->getFinishUrl($deliveryExecutionMock);
     }
 
-    public function testGetFnishUrlReturnsCorrectUrl(): void
+    public function testGetFinishUrlReturnsCorrectUrl(): void
     {
         $expectedUrl = 'http://www.FAKE_RETURN_URL.com/';
 
