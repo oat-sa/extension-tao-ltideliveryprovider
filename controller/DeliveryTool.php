d4ce8acccc9d51a19a21f8e572e09c568250135c
Héctor Arroyo <hector.arroyo@taotesting.com>
2024-01-25 16:13:31 +0100

    Merge pull request #377 from oat-sa/feat/TR-5911-adjust-server-timers-when-resuming-an-execution
    feat: Adjust server-side timers when resuming an execution

--- controller/DeliveryTool.php
+++ controller/DeliveryTool.php
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2013-2021 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ * Copyright (c) 2013-2024 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
  */
 
 namespace oat\ltiDeliveryProvider\controller;
@@ -140,6 +140,10 @@ class DeliveryTool extends ToolModule
 
                         if ($activeExecution instanceof DeliveryExecution) {
                             $this->getConcurringSessionService()->pauseConcurrentSessions($activeExecution);
+
+                            if ($activeExecution->getState()->getUri() === DeliveryExecution::STATE_PAUSED) {
+                                $this->getConcurringSessionService()->adjustTimers($activeExecution);
+                            }
                         }
 
                         $this->resetDeliveryExecutionState($activeExecution);
